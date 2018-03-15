3298707c9b0a32d6063b2d74b6b2a18153a1b999
Cameron <camlecuyer@yahoo.com>
2018-03-15 15:05:00 -0500

    Update

--- ICP_9/Source/Tutorial9/search.js
+++ ICP_9/Source/Tutorial9/search.js
@@ -2,16 +2,16 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = "mongodb://cred:Mon_lock@ds115219.mlab.com:15219/clock";
 
-var findUserwithName = function(db,callback) {
+var findUserwithMobile = function(db,callback) {
     var cursor = db.collection('users').find({"Mobile":"913-555-5555"});
     cursor.each(function(err,doc) {
         assert.equal(err,null);
         if(doc != null)
         {
-            console.log("ID:" + doc.UserID);
-            console.log("First Name:" + doc.FirstName);
-            console.log("Last Name:" + doc.LastName);
-            console.log("City:" + doc.City);
+            console.log("ID: " + doc.UserID);
+            console.log("First Name: " + doc.FirstName);
+            console.log("Last Name: " + doc.LastName);
+            console.log("City: " + doc.City);
         }
     });
 }
@@ -21,7 +21,7 @@ MongoClient.connect(url, function (err, client) {
     console.log("Connected to db");
 
     var db = client.db('clock');
-    findUserwithName(db, function() {
+    findUserwithMobile(db, function() {
         client.close();
     });
 });
